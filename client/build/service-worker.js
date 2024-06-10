ffea6c10e675cd816d4972c197bce7ccb5f77ec9
Kushan Abeysinghe <kushjayamal@gmail.com>
2024-06-10 21:07:54 +0530

    Add Project

---
+++ client/build/service-worker.js
@@ -0,0 +1,14 @@
+// public/service-worker.js
+self.addEventListener('push', function(event) {
+    const data = event.data.json();
+    const options = {
+      body: data.body,
+      icon: 'Logo.png',
+      badge: 'Logo.png'
+    };
+  
+    event.waitUntil(
+      self.registration.showNotification(data.title, options)
+    );
+  });
+  
